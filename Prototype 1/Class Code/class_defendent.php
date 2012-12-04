637c2d8b9da58357b3739b5d1d969d6a24fda8f5
Andrew Thompson <andrew.thompson@mines.sdsmt.edu>
2012-12-03 20:48:18 -0700

    Prototype 1: fixed filename

--- Prototype 1/Class Code/class_defendent.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Defendent {
-	private $firstName;	private $lastName;	private $middleInitial;	private $citationNumber;	private $citationDate;	private $citationTime;	private $phoneNumber;	private $dateOfBirth;	private $courtCaseNumber;	private $agencyNumber;	private $streetAddress;	private $mailingAddress;	private $city;	private $state;	private $zipCode;	private $intakeDate;	private $intakeTime;	private $intakeInterviewer;	private $intakeRefertoJuvenile;	private $intakeDismissed;
-	public function __construct();		private function assignCourt();	private function expungeFull();	private function expungePartial();	private function expungeSeal();	private function printDocket();
-}
-?>
