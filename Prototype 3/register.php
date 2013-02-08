f85e647694dffbdd46d3c5aefd0fc4945f281bd5
amthompson <andrew.thompson@mines.sdsmt.edu>
2013-02-08 01:12:39 -0700

    More registration, validation, captcha work

--- Prototype 3/register.php
+++ Prototype 3/register.php
@@ -14,33 +14,6 @@ $error = 0;
 
 <? 
 if( !$submit ) { 
-
-		// check email
-
-		// check captcha
-
-
-		$user = new User();
-		if( $user->emailExists( $_POST["email"] ) )
-		{
-			$error = 1;
-		}
-		
-		$privatekey = "6Le_gNsSAAAAAOZkcUElnPjfuceX6fmOFcJgTqB9";
-		$resp = recaptcha_check_answer ($privatekey,
-																	$_SERVER["REMOTE_ADDR"],
-																	$_POST["recaptcha_challenge_field"],
-																	$_POST["recaptcha_response_field"]);
-		
-		/*
-		if (!$resp->is_valid) {
-			// incorrect CAPTCHA
-			die ("The reCAPTCHA wasn't entered correctly. Go back and try it again.");
-		} else {
-		// successful verification
-		}
-		*/		
-}
 ?>
 <script>
 jQuery(function($)
@@ -59,18 +32,30 @@ jQuery(function($)
 		},
 		rules: {
 			code: { required: true },
-			password1: { required: true },
-			email: { required: true, email: true }
+			password1: { required: true, minlength: 6 },
+			password2: { required: true, minlength: 6, equalTo: "#password1" },
+			email: { required: true, email: true, remote: "/includes/check_email.php" },
+			recaptcha_response_field: { required: true }
 		}
 	});
 });
 </script>
 
+
+
 <script type="text/javascript">
- var RecaptchaOptions = {
-    theme : 'clean'
- };
- </script>
+
+var RecaptchaOptions = {
+    theme : 'custom',
+    custom_theme_widget: 'recaptcha_widget'
+};
+
+ function showRecaptcha(element) {
+	 Recaptcha.create("6Le_gNsSAAAAAMQuZZBUxdtnFeSWqNLW_AwAxEc4", element, {
+		 theme: "custom",
+		 callback: Recaptcha.focus_response_field});
+ }
+</script>
 
 <div class="ui-state-highlight ui-corner-all" style="margin-top: 10px; padding: 0 .7em;">
 <p><span class="ui-icon ui-icon-info" style="float: left; margin-right: .3em;"></span>
@@ -106,10 +91,28 @@ Passwords should contain a combination of letters (both upper and lower case) an
 		<tr>
 			<td align="right" valign="top" style="padding-bottom:3px;">Prove you are a real person!<br>Retype the characters from the picture</td>			
 			<td>
-				<?php 
-  				$publickey = "6Le_gNsSAAAAAMQuZZBUxdtnFeSWqNLW_AwAxEc4"; 
-  				echo recaptcha_get_html($publickey);
-				?>
+        <div id="recaptcha_widget" style="display:none">
+
+         <div id="recaptcha_image" style="border: 1px solid #ababab"></div>
+         <div class="recaptcha_only_if_incorrect_sol" style="color:red">Incorrect please try again</div>
+            
+         <input type="text" id="recaptcha_response_field" name="recaptcha_response_field" class="wide" style="margin-top: 5px;" />
+      
+         <div><a href="javascript:Recaptcha.reload()">Can't read it? Get another CAPTCHA</a></div>
+         <div class="recaptcha_only_if_audio">
+         	<a href="javascript:Recaptcha.switch_type('image')">Get an image CAPTCHA</a>
+         	<a href="javascript:Recaptcha.showhelp()">Help</a>
+         </div>
+      
+       </div>
+       
+       <script type="text/javascript" src="http://www.google.com/recaptcha/api/challenge?k=6Le_gNsSAAAAAMQuZZBUxdtnFeSWqNLW_AwAxEc4"></script>
+			 <noscript>
+				 <iframe src="http://www.google.com/recaptcha/api/noscript?k=6Le_gNsSAAAAAMQuZZBUxdtnFeSWqNLW_AwAxEc4" height="300" width="500" frameborder="0"></iframe><br>
+				 <textarea name="recaptcha_challenge_field" rows="3" cols="40"></textarea>
+				 <input type="hidden" name="recaptcha_response_field"	value="manual_challenge">
+			 </noscript>
+       
 			</td>
 		</tr>
 		<tr>
@@ -119,7 +122,28 @@ Passwords should contain a combination of letters (both upper and lower case) an
 	</table>
 	</fieldset>
 </form>
+<?
+}
+else
+{
 	
+		$privatekey = "6Le_gNsSAAAAAOZkcUElnPjfuceX6fmOFcJgTqB9";
+		$resp = recaptcha_check_answer ($privatekey,
+																		$_SERVER["REMOTE_ADDR"],
+																		$_REQUEST["recaptcha_challenge_field"],
+																		$_REQUEST["recaptcha_response_field"]);
+		
+		
+		if (!$resp->is_valid) {
+			die("WRONG!");
+		} else {
+		
+			echo "OK!";
+		}
+		
+}
+?>
+
 <?php  
 include($_SERVER['DOCUMENT_ROOT']."/includes/footer_external.php");
 ?>
