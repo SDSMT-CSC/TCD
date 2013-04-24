5b5029ed4896c4f4ca3d138ef96794c8a1f3606b
Andrew Thompson <andrew.thompson@mines.sdsmt.edu>
2013-04-24 02:00:45 -0600

    File cleanup / updated password strength

--- Prototype 4/data/defendants_current.php
+++
@@ -1,9 +0,0 @@
-<?php
-header("Content-Type: application/json");
-include($_SERVER['DOCUMENT_ROOT']."/includes/secure.php");
-include($_SERVER['DOCUMENT_ROOT']."/includes/class_data.php");
-
-//pulls the data, and gets the table
-$data = new Data();
-echo $data->fetchDefendantListing( $user_programID );
-?>
